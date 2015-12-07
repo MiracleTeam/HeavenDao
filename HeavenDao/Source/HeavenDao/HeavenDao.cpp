90bf5be93f35a9b5ece2e4415e2ea185cdac2c6e
ituuz <yue19870813@163.com>
2015-12-07 15:01:11 +0800

    First commit.

---
+++ HeavenDao/Source/HeavenDao/HeavenDao.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HeavenDao.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HeavenDao, "HeavenDao" );
---
+++ HeavenDao/Source/HeavenDao/HeavenDaoGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HeavenDaoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEAVENDAO_API AHeavenDaoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
